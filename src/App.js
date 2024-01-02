eb9566dfe9d7df3dc2a1ad6b8ed9e49ebfa9c983
Vikrant Gaherwar <vikrant.gaherwar@inadev.com>
2024-01-02 18:12:32 +0530

    fifth 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} /> */}
         <Route path="/" element={<Header />}>
           <Route path="home" element={<Home />} />
           <Route path="about" element={<>about</>} />
