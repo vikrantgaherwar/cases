760eab6fc45a28eb8c1439094f285429da4837b6
Vikrant Gaherwar <vikrant.gaherwar@inadev.com>
2024-01-02 17:18:51 +0530

    sec commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import Login from "./component/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<Login />} /> */}
-        <Route path="/" element={<Header />}>
-          <Route path="home" element={<Login />} />
-          <Route path="about" element={<>about</>} />
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     {/* <Route path="/" element={<Login />} /> */}
+    //     <Route path="/" element={<Header />}>
+    //       <Route path="home" element={<Login />} />
+    //       <Route path="about" element={<>about</>} />
+    //     </Route>
+    //     <Route path="*" element={<ErrorPage />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <Login />
   );
 }
 
